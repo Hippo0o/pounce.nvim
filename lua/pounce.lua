cd14f1237d471bc5999e0e02fa441f50987576c8
Tobias S <seifert@devlabor.com>
2022-06-27 16:05:48 +0200

    fix highlights not working with PounceUnmatched PounceGap and PounceMatch get overwritten by PounceUnmatched highlights

--- lua/pounce.lua
+++ lua/pounce.lua
@@ -53,6 +53,7 @@ function M.pounce(opts)
   local windows = get_windows(opts)
   local ns = vim.api.nvim_create_namespace ""
   local input = opts and opts.do_repeat and last_input or ""
+  local hl_prio = 65533
 
   while true do
     local start_clock = os.clock()
@@ -80,6 +81,7 @@ function M.pounce(opts)
         end_line = win_info.botline,
         hl_group = "PounceUnmatched",
         hl_eol = true,
+        priority = hl_prio,
       })
     end
 
@@ -134,16 +136,17 @@ function M.pounce(opts)
         local seen_key = string.format("%d.%d.%d", buf, hit.line, hit.indices[1])
         if seen[seen_key] == nil then
           seen[seen_key] = true
-          vim.api.nvim_buf_add_highlight(
-            buf,
-            ns,
-            "PounceGap",
-            hit.line - 1,
-            hit.indices[1] - 1,
-            hit.indices[#hit.indices] - 1
-          )
+          vim.api.nvim_buf_set_extmark(buf, ns, hit.line - 1, hit.indices[1] - 1, {
+            end_col = hit.indices[#hit.indices] - 1,
+            hl_group = "PounceGap",
+            priority = hl_prio,
+          })
           for _, index in ipairs(hit.indices) do
-            vim.api.nvim_buf_add_highlight(buf, ns, "PounceMatch", hit.line - 1, index - 1, index)
+            vim.api.nvim_buf_set_extmark(buf, ns, hit.line - 1, index - 1, {
+              end_col = index,
+              hl_group = "PounceMatch",
+              priority = hl_prio,
+            })
           end
 
           local accept_keys = getconfig("accept_keys", opts)
